9bb0eeaaac3d4f07d87a3ae4f5674826787160e8
MikeyHF <56313204+MikeyHF@users.noreply.github.com>
2026-01-14 15:00:46 -0800

    New form for data infra demo

---
+++ Lab 5 Code/C#/Data Infrastructure Form/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Data_Infrastructure_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxWebCam = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnCanny = new System.Windows.Forms.Button();
+            this.btnFPS = new System.Windows.Forms.Button();
+            this.pictureBoxEffect = new System.Windows.Forms.PictureBox();
+            this.btnHough = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEffect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxWebCam
+            // 
+            this.pictureBoxWebCam.Location = new System.Drawing.Point(97, 163);
+            this.pictureBoxWebCam.Name = "pictureBoxWebCam";
+            this.pictureBoxWebCam.Size = new System.Drawing.Size(661, 575);
+            this.pictureBoxWebCam.TabIndex = 0;
+            this.pictureBoxWebCam.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(178, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(219, 46);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(403, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(219, 46);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnCanny
+            // 
+            this.btnCanny.Location = new System.Drawing.Point(937, 12);
+            this.btnCanny.Name = "btnCanny";
+            this.btnCanny.Size = new System.Drawing.Size(219, 46);
+            this.btnCanny.TabIndex = 3;
+            this.btnCanny.Text = "Canny";
+            this.btnCanny.UseVisualStyleBackColor = true;
+            this.btnCanny.Click += new System.EventHandler(this.btnCanny_Click);
+            // 
+            // btnFPS
+            // 
+            this.btnFPS.Location = new System.Drawing.Point(712, 12);
+            this.btnFPS.Name = "btnFPS";
+            this.btnFPS.Size = new System.Drawing.Size(219, 46);
+            this.btnFPS.TabIndex = 4;
+            this.btnFPS.Text = "FPS";
+            this.btnFPS.UseVisualStyleBackColor = true;
+            this.btnFPS.Click += new System.EventHandler(this.btnFPS_Click);
+            // 
+            // pictureBoxEffect
+            // 
+            this.pictureBoxEffect.Location = new System.Drawing.Point(866, 163);
+            this.pictureBoxEffect.Name = "pictureBoxEffect";
+            this.pictureBoxEffect.Size = new System.Drawing.Size(661, 575);
+            this.pictureBoxEffect.TabIndex = 5;
+            this.pictureBoxEffect.TabStop = false;
+            // 
+            // btnHough
+            // 
+            this.btnHough.Location = new System.Drawing.Point(1162, 12);
+            this.btnHough.Name = "btnHough";
+            this.btnHough.Size = new System.Drawing.Size(219, 46);
+            this.btnHough.TabIndex = 6;
+            this.btnHough.Text = "Hough Circle";
+            this.btnHough.UseVisualStyleBackColor = true;
+            this.btnHough.Click += new System.EventHandler(this.btnHough_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1691, 825);
+            this.Controls.Add(this.btnHough);
+            this.Controls.Add(this.pictureBoxEffect);
+            this.Controls.Add(this.btnFPS);
+            this.Controls.Add(this.btnCanny);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBoxWebCam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEffect)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxWebCam;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnCanny;
+        private System.Windows.Forms.Button btnFPS;
+        private System.Windows.Forms.PictureBox pictureBoxEffect;
+        private System.Windows.Forms.Button btnHough;
+    }
+}
+
---
+++ Lab 5 Code/C#/Data Infrastructure Form/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Data_Infrastructure_Form
+{
+    public partial class Form1 : Form
+    {
+        private bool _run = false;
+        private bool _canny = false;
+        private bool _hough = false;
+        private VideoCapture _capture;
+        private Mat _image;
+        private Thread _cameraThread;
+        private bool _fps = false;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture(1);
+            _image = new Mat();
+            _cameraThread = new Thread(new ThreadStart(CaptureCameraCallback));
+            _cameraThread.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _cameraThread.Interrupt();
+            _capture.Release();
+            
+        }
+
+        private void CaptureCameraCallback()
+        {
+            while (true)
+            {
+                if (!_run) continue;
+                var startTime = DateTime.Now;
+
+                _capture.Read(_image);
+                if (_image.Empty()) return;
+                var imageRes = new Mat();
+                var newImageGrey = new Mat();
+                Cv2.Resize(_image, imageRes, new OpenCvSharp.Size(320, 240));
+                var newImage = imageRes.Clone();
+                if (_canny)
+                    Cv2.Canny(imageRes, newImage, 50, 200);
+                if (_hough)
+                {
+                    Cv2.MedianBlur(newImage, newImage, 5);
+                    Cv2.CvtColor(newImage, newImageGrey, ColorConversionCodes.BGR2GRAY);
+                    var circles = Cv2.HoughCircles(newImageGrey, HoughModes.Gradient, 1, 20);
+
+                    foreach (var circle in circles)
+                    {
+                        Cv2.Circle(newImage, (int) circle.Center.X, (int) circle.Center.Y, (int) circle.Radius, Scalar.Green, 5);
+                        Cv2.Circle(newImage, (int)circle.Center.X, (int)circle.Center.Y, 5, Scalar.Red, -1);
+                    }
+
+                }
+
+                if (_fps)
+                {
+                    var diff = DateTime.Now - startTime;
+                    var fpsInfo = $"FPS: Nan";
+                    if (diff.Milliseconds > 0)
+                    {
+                        var fpsVal = 1.0 / diff.Milliseconds * 1000;
+                        fpsInfo = $"FPS: {fpsVal:00}";
+                    }
+                    Cv2.PutText(imageRes, fpsInfo, new OpenCvSharp.Point(10, 20), HersheyFonts.HersheyComplexSmall, 1, Scalar.Black);
+                }
+
+                var bmpWebCam = BitmapConverter.ToBitmap(imageRes);
+                var bmpEffect = BitmapConverter.ToBitmap(newImage);
+
+                pictureBoxWebCam.Image = bmpWebCam;
+                pictureBoxEffect.Image = bmpEffect;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            _run = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _run = false;
+        }
+
+        private void btnCanny_Click(object sender, EventArgs e)
+        {
+            _hough = false;
+            _canny = !_canny;
+
+        }
+
+        private void btnFPS_Click(object sender, EventArgs e)
+        {
+            _fps = !_fps;
+        }
+
+        private void btnHough_Click(object sender, EventArgs e)
+        {
+            _canny = false;
+            _hough = !_hough;
+            
+        }
+    }
+}
