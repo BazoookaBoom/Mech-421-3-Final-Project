146931078139b6a4c23aadb00ba7d139a213ca66
Conor <123661795+BazoookaBoom@users.noreply.github.com>
2026-01-28 15:18:45 -0800

    Have to commit these. Good luck

---
+++ Labs/Lab 6 Code/C#/MECH423Lab6Ex4/MECH423Lab6Ex4/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace MECH423Lab6Ex4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxCOMPorts = new System.Windows.Forms.ComboBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.PWMSetTextBox = new System.Windows.Forms.TextBox();
+            this.SetPWMButton = new System.Windows.Forms.Button();
+            this.PositionTextBox = new System.Windows.Forms.TextBox();
+            this.VelocityTextBox = new System.Windows.Forms.TextBox();
+            this.HzTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived +=
+                new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+
+            // 
+            // comboBoxCOMPorts
+            // 
+            this.comboBoxCOMPorts.FormattingEnabled = true;
+            this.comboBoxCOMPorts.Items.AddRange(new object[] {
+            "COM3",
+            "COM7"});
+            this.comboBoxCOMPorts.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxCOMPorts.Name = "comboBoxCOMPorts";
+            this.comboBoxCOMPorts.Size = new System.Drawing.Size(121, 21);
+
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(150, 12);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click +=
+                new System.EventHandler(this.ConnectButton_Click);
+
+            // 
+            // PWMSetTextBox
+            // 
+            this.PWMSetTextBox.Location = new System.Drawing.Point(12, 55);
+            this.PWMSetTextBox.Name = "PWMSetTextBox";
+            this.PWMSetTextBox.Size = new System.Drawing.Size(75, 20);
+            this.PWMSetTextBox.Text = "0";
+
+            // 
+            // SetPWMButton
+            // 
+            this.SetPWMButton.Location = new System.Drawing.Point(100, 53);
+            this.SetPWMButton.Name = "SetPWMButton";
+            this.SetPWMButton.Size = new System.Drawing.Size(75, 23);
+            this.SetPWMButton.Text = "Set PWM";
+            this.SetPWMButton.UseVisualStyleBackColor = true;
+            this.SetPWMButton.Click +=
+                new System.EventHandler(this.SetPWMButton_Click);
+
+            // 
+            // PositionTextBox
+            // 
+            this.PositionTextBox.Location = new System.Drawing.Point(12, 100);
+            this.PositionTextBox.Name = "PositionTextBox";
+            this.PositionTextBox.Size = new System.Drawing.Size(75, 20);
+            this.PositionTextBox.Text = "0";
+
+            // 
+            // VelocityTextBox
+            // 
+            this.VelocityTextBox.Location = new System.Drawing.Point(100, 100);
+            this.VelocityTextBox.Name = "VelocityTextBox";
+            this.VelocityTextBox.Size = new System.Drawing.Size(75, 20);
+            this.VelocityTextBox.Text = "0";
+
+            // 
+            // HzTextBox
+            // 
+            this.HzTextBox.Location = new System.Drawing.Point(188, 100);
+            this.HzTextBox.Name = "HzTextBox";
+            this.HzTextBox.Size = new System.Drawing.Size(75, 20);
+            this.HzTextBox.Text = "0";
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(280, 150);
+            this.Controls.Add(this.comboBoxCOMPorts);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.PWMSetTextBox);
+            this.Controls.Add(this.SetPWMButton);
+            this.Controls.Add(this.PositionTextBox);
+            this.Controls.Add(this.VelocityTextBox);
+            this.Controls.Add(this.HzTextBox);
+            this.Name = "Form1";
+            this.Text = "Lab6Ex3 - Logger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxCOMPorts;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox PWMSetTextBox;
+        private System.Windows.Forms.Button SetPWMButton;
+        private System.Windows.Forms.TextBox PositionTextBox;
+        private System.Windows.Forms.TextBox VelocityTextBox;
+        private System.Windows.Forms.TextBox HzTextBox;
+    }
+}
---
+++ Labs/Lab 6 Code/C#/MECH423Lab6Ex4/MECH423Lab6Ex4/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace MECH423Lab6Ex4
+{
+    public partial class Form1 : Form
+    {
+        // ===== Logging =====
+        StreamWriter logWriter;
+        Stopwatch stopwatch = new Stopwatch();
+        volatile bool logging = false;
+
+        // ===== Encoder =====
+        int lastCount = 0;
+        double lastTime = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            serialPort1.DataReceived += serialPort1_DataReceived;
+
+            // Populate COM ports
+            comboBoxComPort.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        // ================= CONNECT =================
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = comboBoxComPort.Text;
+                serialPort1.BaudRate = 115200;
+                serialPort1.Open();
+            }
+        }
+
+        // ================= SEND PWM STEP =================
+        private void buttonSendPWM_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen) return;
+
+            int pwm = int.Parse(textBoxPWM.Text); // 0–65535
+            byte msb = (byte)((pwm >> 8) & 0xFF);
+            byte lsb = (byte)(pwm & 0xFF);
+
+            byte[] packet = { 0xAA, msb, lsb };
+            serialPort1.Write(packet, 0, packet.Length);
+        }
+
+        // ================= START LOGGING =================
+        private void buttonStartLog_Click(object sender, EventArgs e)
+        {
+            logWriter = new StreamWriter("motor_log.csv");
+            logWriter.WriteLine("Time_s,Position_counts,Velocity_counts_per_s");
+
+            lastCount = 0;
+            lastTime = 0;
+
+            stopwatch.Restart();
+            logging = true;
+        }
+
+        // ================= STOP LOGGING =================
+        private void buttonStopLog_Click(object sender, EventArgs e)
+        {
+            logging = false;
+            stopwatch.Stop();
+
+            logWriter.Flush();
+            logWriter.Close();
+        }
+
+        // ================= SERIAL RX =================
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            while (serialPort1.BytesToRead >= 4)
+            {
+                if (serialPort1.ReadByte() != 0xAA)
+                    continue;
+
+                int dir = serialPort1.ReadByte();
+                int msb = serialPort1.ReadByte();
+                int lsb = serialPort1.ReadByte();
+
+                int count = (msb << 8) | lsb;
+                if (dir == 0) count = -count;
+
+                double t = stopwatch.Elapsed.TotalSeconds;
+
+                double vel = 0;
+                if (lastTime > 0)
+                    vel = (count - lastCount) / (t - lastTime);
+
+                lastCount = count;
+                lastTime = t;
+
+                if (logging)
+                {
+                    logWriter.WriteLine($"{t:F6},{count},{vel:F3}");
+                }
+            }
+        }
+    }
+}
