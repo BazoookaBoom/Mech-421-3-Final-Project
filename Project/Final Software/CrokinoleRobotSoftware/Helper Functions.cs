5b377cb7c886d4a7eaea5b12bb56f25502830e48
MikeyHF <56313204+MikeyHF@users.noreply.github.com>
2026-02-14 03:18:59 -0800

    New Form
    Created a new form for the final project deliverable

---
+++ Project/Final Software/CrokinoleRobotSoftware/Helper Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrokinoleRobotSoftware
+{
+    internal class HelperFxns
+    {
+        // Connect to the MCU over UART serial communications
+        private void ConnectMCU()
+        {
+
+        }
+
+        // Connect to the camera
+        private void ConnectCamera()
+        {
+
+        }
+
+
+    }
+}
