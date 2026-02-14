d0d6d813b3c96c79deb7f38ba3245fcc3ad9f7cb
Conor <123661795+BazoookaBoom@users.noreply.github.com>
2026-02-14 15:32:44 -0800

    Create FinalRobotControl.ino

---
+++ Project/Firmware/FinalRobotControl/FinalRobotControl.ino
@@ -0,0 +1,29 @@
+/*
+Name: FinalRobotControl.ino
+Created By: CO
+Created On: November 25, 2025
+Updated On: November 30, 2025
+Description: Full final robot code for the Mech 423 Final Project
+Note: Godspeed
+      We are using an Arduino Uno
+      Stepper, TB6600
+      Solenoid
+      Servo
+      Limit Switches
+*/
+
+/* Revision Tracking
+ DD/MM/YY Rev	Name  Changes
+ 14/02/26 A		CO		Made initial version, will focus on making solenoid control work
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
